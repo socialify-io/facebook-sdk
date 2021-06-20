d860a222d9757f0e6999260f3b7a5c8652dec3ef
Pengwius <tomekftomekf@gmail.com>
2021-06-20 21:30:05 +0200

    Add comments to request helper

--- Sources/FacebookSdk/Helpers/requestHelpers.swift
+++ Sources/FacebookSdk/Helpers/requestHelpers.swift
@@ -12,8 +12,13 @@ extension FacebookClient {
     /// MARK: - Request helpers
     func makeRequest(request: URLRequest, completion: @escaping (Result<[String: Any], FacebookError>) -> Void) {
         var request = request
+        
+        // Adding User-Agent
         request.addValue("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36", forHTTPHeaderField: "User-Agent")
+        
+        // Sending request
         session?.dataTask(with: request) { (data, response, error) in
+            // Looking for errors
             guard let data = data else {
                   if let _ = error {
                     completion(.failure(FacebookError.ConnectionError))
@@ -23,6 +28,7 @@ extension FacebookClient {
                 return
                 }
             
+            // Preparing return data
             let returnBody: [String: Any] = [
                 "data": data,
                 "response": response
