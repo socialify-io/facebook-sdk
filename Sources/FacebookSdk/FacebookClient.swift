e3c04383212d00cd24f9761acb61e44bfd8eecc9
Pengwius <tomekftomekf@gmail.com>
2021-06-20 19:14:06 +0200

    Logging in

--- Sources/FacebookSdk/FacebookClient.swift
+++ Sources/FacebookSdk/FacebookClient.swift
@@ -1,62 +1,93 @@
+//
+//  FacebookClient.swift
+//
+//
+//  Created by Tomasz on 22/05/2021.
+//
+
 import Foundation
 import SwiftSoup
 
 public class FacebookClient {
     
-    public init() {}
-    
-    public func login(email: String, password: String) {
-        getPayload(email: email)
+    public init() {
+        self.myDelegate = true ? MySession() : nil
+        self.session = URLSession(configuration: URLSessionConfiguration.default, delegate: myDelegate, delegateQueue: nil)
     }
     
-    private func getPayload(email: String) {
-        let requiredInputs = ["jazoest", "lsd", "legacy_return", "trynum", "lgnrnd", "had_cp_prefilled", "had_password_prefilled"]
-        
-        var payload: [String: Any] = [
-            "jazoest": "",
-            "lsd": "",
-            "display": "",
-            "enable_profile_selector": "",
-            "isprivate": "",
-            "legacy_return": "",
-            "profile_selector_ids": "",
-            "return_session": "",
-            "skip_api_login": "",
-            "signed_next": "",
-            "trynum": "",
-            "timezone": "",
-            "lgndim": "",
-            "lgnrnd": "",
-            "lgnjs": "",
+    public let LIBRARY_VERSION = "v0.0.1"
+    public var myDelegate: MySession?
+
+    public var session: URLSession?
+    
+    /// MARK: - Public functions
+    
+    /// Logging in
+    /// Parameters:
+    ///  - email
+    ///  - password
+    /// Returns
+    /// - true or false
+    
+    public func login(email: String, password: String, completion: @escaping (Result<Bool, FacebookError>) -> Void) {
+        // Preparing payload
+        let payload: [String: Any] = [
+            "jazoest": 2935,
+            "lsd": "AVpbphIq-K4",
+            "login_source": "comet_headerless_login",
+            "next": "",
             "email": email,
-            "prefill_contact_point": email,
-            "prefill_source": "",
-            "prefill_type": "",
-            "first_prefill_source": "",
-            "first_prefill_type": "",
-            "had_cp_prefilled": "",
-            "had_password_prefilled": "",
-            "ab_test_data": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/AAAAAAAAAAAA",
-            "encpass": ""
+            "pass": password
         ]
         
-        do {
-            let content: String = try String(contentsOf: URL(string: "https://www.facebook.com/login")!)
-            let doc: Document = try SwiftSoup.parseBodyFragment(content)
-            
-            let inputs = try doc.select("input")
-
-            try inputs.forEach { input in
-                if (requiredInputs.contains(try input.attr("name"))) {
-                    payload[try input.attr("name")] = try input.attr("value")
+        // Creating privacy mutation token
+        let timestamp = Int64((Date().timeIntervalSince1970))
+        let privacyMutationToken = Data("{\"type\":0,\"creation_time\":\(timestamp),\"callsite_id\":381229079575946}".utf8).base64EncodedString()
+        
+        // Preparing request
+        let url = URL(string: "https://www.facebook.com/login/?privacy_mutation_token=\(privacyMutationToken)")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = Data(payload.map { "\($0.key)=\($0.value)" }.joined(separator: "&").utf8)
+        
+        // Preparing headers
+        request.allHTTPHeaderFields = [
+            "Cookie": "datr=QfbNYK0kA7zXWagrFbytAswt; sb=V_bNYEf2HisBhggP9qDT8fZL; dpr=2; locale=en_GB; wd=876x748; fr=1fj24NQhQ3bYG9HVz.AWVcO1tgdR19ey3Y8wc-RMkxYII.Bgzmbb.q4.AAA.0.0.Bgzmcy.AWXaBBYWli8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "connection": "close",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+        ]
+        
+        // Send request
+        session?.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse {
+                // Checking is logged in
+                if(response.statusCode == 302) {
+                    completion(.success(true))
+                } else {
+                    completion(.success(false))
                 }
             }
-            
-            print(payload)
-        } catch Exception.Error(let type, let message) {
-            print(message)
-        } catch {
-            print("error")
-        }
+        }.resume()
+    }
+    
+    public enum FacebookError: Error {
+        case ConnectionError
+        case UnexpectedError
+    }
+}
+
+public class MySession: NSObject, URLSessionTaskDelegate {
+
+    public func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
+        completionHandler(nil)
     }
 }
