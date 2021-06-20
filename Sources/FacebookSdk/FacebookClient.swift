bd4d6642be5f2dab4c441bcbd1b9a4d60e479b9d
Tomasz F <tomekftomekf@gmail.com>
2021-06-20 21:54:39 +0200

    Code cleanup (#2)
    * Comments
    
    * Sort files
    
    * Add request helper
    
    * Use request helper
    
    * Add comments to request helper
    
    * Rename and move Session helper class

--- Sources/FacebookSdk/FacebookClient.swift
+++ Sources/FacebookSdk/FacebookClient.swift
@@ -6,88 +6,25 @@
 //
 
 import Foundation
-import SwiftSoup
 
 public class FacebookClient {
     
+    /// MARK: - Public variables
+    
     public init() {
-        self.myDelegate = true ? MySession() : nil
-        self.session = URLSession(configuration: URLSessionConfiguration.default, delegate: myDelegate, delegateQueue: nil)
+        self.delegate = true ? Session() : nil
+        self.session = URLSession(configuration: URLSessionConfiguration.default, delegate: delegate, delegateQueue: nil)
     }
     
     public let LIBRARY_VERSION = "v0.0.1"
-    public var myDelegate: MySession?
-
+    
+    public var delegate: Session?
     public var session: URLSession?
     
-    /// MARK: - Public functions
-    
-    /// Logging in
-    /// Parameters:
-    ///  - email
-    ///  - password
-    /// Returns
-    /// - true or false
-    
-    public func login(email: String, password: String, completion: @escaping (Result<Bool, FacebookError>) -> Void) {
-        // Preparing payload
-        let payload: [String: Any] = [
-            "jazoest": 2935,
-            "lsd": "AVpbphIq-K4",
-            "login_source": "comet_headerless_login",
-            "next": "",
-            "email": email,
-            "pass": password
-        ]
-        
-        // Creating privacy mutation token
-        let timestamp = Int64((Date().timeIntervalSince1970))
-        let privacyMutationToken = Data("{\"type\":0,\"creation_time\":\(timestamp),\"callsite_id\":381229079575946}".utf8).base64EncodedString()
-        
-        // Preparing request
-        let url = URL(string: "https://www.facebook.com/login/?privacy_mutation_token=\(privacyMutationToken)")!
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = Data(payload.map { "\($0.key)=\($0.value)" }.joined(separator: "&").utf8)
-        
-        // Preparing headers
-        request.allHTTPHeaderFields = [
-            "Cookie": "datr=QfbNYK0kA7zXWagrFbytAswt; sb=V_bNYEf2HisBhggP9qDT8fZL; dpr=2; locale=en_GB; wd=876x748; fr=1fj24NQhQ3bYG9HVz.AWVcO1tgdR19ey3Y8wc-RMkxYII.Bgzmbb.q4.AAA.0.0.Bgzmcy.AWXaBBYWli8",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Upgrade-Insecure-Requests": "1",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-User": "?1",
-            "Sec-Fetch-Dest": "document",
-            "connection": "close",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
-        ]
-        
-        // Send request
-        session?.dataTask(with: request) { (data, response, error) in
-            if let response = response as? HTTPURLResponse {
-                // Checking is logged in
-                if(response.statusCode == 302) {
-                    completion(.success(true))
-                } else {
-                    completion(.success(false))
-                }
-            }
-        }.resume()
-    }
+    /// MARK: - Facebook Errors
     
     public enum FacebookError: Error {
         case ConnectionError
         case UnexpectedError
-    }
-}
-
-public class MySession: NSObject, URLSessionTaskDelegate {
-
-    public func urlSession(_ session: URLSession, task: URLSessionTask, willPerformHTTPRedirection response: HTTPURLResponse, newRequest request: URLRequest, completionHandler: @escaping (URLRequest?) -> Void) {
-        completionHandler(nil)
     }
 }
---
+++ Sources/FacebookSdk/Login/login.swift
@@ -0,0 +1,76 @@
+//
+//  login.swift
+//  
+//
+//  Created by Tomasz on 20/06/2021.
+//
+
+import Foundation
+
+extension FacebookClient {
+    
+    /// MARK: - Logging in functions
+    
+    /// Logging in
+    /// Parameters:
+    ///  - email
+    ///  - password
+    /// Returns
+    /// - true or false
+    
+    public func login(email: String, password: String, completion: @escaping (Result<Bool, FacebookError>) -> Void) {
+        // Preparing payload
+        let payload: [String: Any] = [
+            "jazoest": 2935,
+            "lsd": "AVpbphIq-K4",
+            "login_source": "comet_headerless_login",
+            "next": "",
+            "email": email,
+            "pass": password
+        ]
+        
+        // Creating privacy mutation token
+        let timestamp = Int64((Date().timeIntervalSince1970))
+        let privacyMutationToken = Data("{\"type\":0,\"creation_time\":\(timestamp),\"callsite_id\":381229079575946}".utf8).base64EncodedString()
+        
+        // Preparing request
+        let url = URL(string: "https://www.facebook.com/login/?privacy_mutation_token=\(privacyMutationToken)")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = Data(payload.map { "\($0.key)=\($0.value)" }.joined(separator: "&").utf8)
+        
+        // Preparing headers
+        request.allHTTPHeaderFields = [
+            "Cookie": "datr=QfbNYK0kA7zXWagrFbytAswt; sb=V_bNYEf2HisBhggP9qDT8fZL; dpr=2; locale=en_GB; wd=876x748; fr=1fj24NQhQ3bYG9HVz.AWVcO1tgdR19ey3Y8wc-RMkxYII.Bgzmbb.q4.AAA.0.0.Bgzmcy.AWXaBBYWli8",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+            "connection": "close",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+        ]
+        
+        // Send request
+        self.makeRequest(request: request) { result in
+            switch result {
+            case .success(let result):
+                let response = result["response"]
+                if let response = response as? HTTPURLResponse {
+                    // Checking is logged in
+                    if(response.statusCode == 302) {
+                        completion(.success(true))
+                    } else {
+                        completion(.success(false))
+                    }
+                }
+            
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
